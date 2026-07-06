80222307d513c2b38ffceef2d3dd14b0b2144739
rwong <rwong@noreply.codeberg.org>
2026-07-06 16:33:18 +0200

    Add drawing ability

--- src/main.rs
+++ src/main.rs
@@ -1,137 +1,136 @@
-use std::io::{self, Cursor, Write, stdout};
+use std::io::{self, Write, stdout};
 
 use crossterm::{
-    ExecutableCommand, QueueableCommand, cursor, queue,
-    style::{self, Stylize},
+    ExecutableCommand, QueueableCommand,
+    cursor::{self, MoveRight, MoveToColumn, MoveToNextLine},
+    style::{self, Print, PrintStyledContent, Stylize},
     terminal,
 };
+use rustix::system::uname;
 
 fn main() -> anyhow::Result<()> {
     let mut output = stdout();
 
+    let dev_info = get_device_info()?;
+
+    let (cols, t_col) = (48, 12);
+
     output.execute(terminal::Clear(terminal::ClearType::All))?;
+    output.queue(cursor::MoveTo(0, 0))?;
 
-    let (rows, cols, t_col) = (24, 64, 12);
-
-    let header_top = |x: u16, output: &mut dyn Write| -> io::Result<()> {
-        print_divider(output, x, cols, '┌', '┬', '┐')
-    };
-
-    let header_bottom = |x: u16, output: &mut dyn Write| -> io::Result<()> {
-        print_divider(output, x, cols, '├', '┴', '┤')
-    };
-
-    let bottom_div = |x: u16, output: &mut dyn Write| -> io::Result<()> {
-        print_divider_with_t(output, x, cols, t_col, '└', '─', '┴', '┘')
-    };
-
-    let content_div = |x: u16, output: &mut dyn Write| -> io::Result<()> {
-        print_divider_with_t(output, x, cols, t_col, '├', '─', '┼', '┤')
-    };
-
-    let content_div_top = |x: u16, output: &mut dyn Write| -> io::Result<()> {
-        print_divider_with_t(output, x, cols, t_col, '├', '─', '┬', '┤')
-    };
-
-    let content_div_middle = |x: u16, output: &mut dyn Write| -> io::Result<()> {
-        print_divider(output, x, cols, '├', '─', '┤')
-    };
-
-    let print_middle_text = |x: u16, output: &mut dyn Write, content: String| -> io::Result<()> {
-        let before_whitespace = (cols as usize - content.chars().count()) / 2;
-        if x == 0 || x == cols - 1 {
-            output.queue(style::Print('│'))?;
-        } else if x as usize == before_whitespace {
-            output.queue(style::PrintStyledContent("test".bold()))?;
-        }
-
-        Ok(())
-    };
-
-    for y in 0..rows {
-        for x in 0..cols {
-            output.queue(cursor::MoveTo(x, y))?;
-            match y {
-                0 => {
-                    header_top(x, &mut output)?;
-                }
-                1 => {
-                    header_bottom(x, &mut output)?;
-                }
-                2 => {
-                    print_middle_text(x, &mut output, "abc".to_string())?;
-                }
-                3 => {
-                    content_div_top(x, &mut output)?;
-                }
-                bottom if bottom == rows - 1 => {
-                    bottom_div(x, &mut output)?;
-                }
-                _ => {
-                    if (y - 3) % 2 == 1 {
-                        output
-                            .queue(style::Print('│'))?
-                            .queue(cursor::MoveTo(2, y))?
-                            .queue(style::PrintStyledContent("asdf".bold()))?
-                            .queue(cursor::MoveTo(t_col, y))?
-                            .queue(style::Print('│'))?
-                            .queue(cursor::MoveRight(1))?
-                            .queue(style::PrintStyledContent("content".italic()))?
-                            .queue(cursor::MoveTo(cols - 1, y))?
-                            .queue(style::Print('│'))?;
-                        break;
-                    } else {
-                        content_div(x, &mut output)?;
-                    }
-                }
-            }
-        }
-    }
+    draw_header(&mut output, cols, t_col)?;
+    draw_device_info(&mut output, t_col, cols, dev_info)?;
+    draw_footer(&mut output, cols, t_col)?;
 
     output.flush()?;
 
     Ok(())
 }
 
-fn print_divider(
-    output: &mut dyn Write,
-    x: u16,
-    cols: u16,
-    l: char,
-    m: char,
-    r: char,
-) -> io::Result<()> {
-    Ok(match x {
-        0 => {
-            output.queue(style::Print(l))?;
-        }
-        right if right == cols - 1 => {
-            output.queue(style::Print(r))?;
-        }
-        _ => {
-            output.queue(style::Print(m))?;
-        }
-    })
+enum RowItem {
+    KV(String, String),
+    LineBreak,
 }
 
-fn print_divider_with_t(
-    output: &mut dyn Write,
-    x: u16,
-    cols: u16,
-    t_col: u16,
-    l: char,
-    m: char,
-    t: char,
-    r: char,
-) -> io::Result<()> {
-    if x == 0 {
-        output.queue(style::Print(l))?;
-    } else if x == t_col {
-        output.queue(style::Print(t))?;
-    } else if x == cols - 1 {
-        output.queue(style::Print(r))?;
-    } else {
-        output.queue(style::Print(m))?;
+fn get_device_info() -> io::Result<Box<[RowItem]>> {
+    let mut res: Vec<RowItem> = Vec::new();
+
+    let uname = uname();
+    res.push(RowItem::KV(
+        "Hostname".to_string(),
+        uname.sysname().to_string_lossy().to_string(),
+    ));
+
+    Ok(res.into_boxed_slice())
+}
+
+fn draw_header(output: &mut dyn Write, width: u16, t_col: u16) -> io::Result<()> {
+    output.queue(Print('┌'))?;
+    for _i in 1..width - 1 {
+        output.queue(Print('┬'))?;
     }
+    output.queue(Print('┐'))?.queue(MoveToNextLine(1))?;
+
+    output.queue(Print('├'))?;
+    for _i in 1..width - 1 {
+        output.queue(Print('┴'))?;
+    }
+    output.queue(Print('┤'))?.queue(MoveToNextLine(1))?;
+
+    let header_title = "Machine Report";
+
+    let center_loc: u16 = width / 2 - header_title.len() as u16 / 2;
+    output
+        .queue(Print('│'))?
+        .queue(MoveToColumn(center_loc))?
+        .queue(PrintStyledContent(header_title.bold()))?
+        .queue(MoveToColumn(width - 1))?
+        .queue(Print('│'))?
+        .queue(MoveToNextLine(1))?;
+
+    output.queue(Print('├'))?;
+    for _i in 1..width - 1 {
+        output.queue(Print('─'))?;
+    }
+
+    output
+        .queue(Print('┤'))?
+        .queue(MoveToColumn(t_col))?
+        .queue(Print('┬'))?
+        .queue(MoveToNextLine(1))?;
+
+    Ok(())
+}
+
+fn draw_footer(output: &mut dyn Write, width: u16, t_col: u16) -> io::Result<()> {
+    output.queue(Print('└'))?;
+    for _i in 1..width - 1 {
+        output.queue(Print('─'))?;
+    }
+    output.queue(Print('┘'))?;
+
+    output.queue(MoveToColumn(t_col))?.queue(Print('┴'))?;
+
+    Ok(())
+}
+
+fn draw_device_info(
+    output: &mut dyn Write,
+    t_col: u16,
+    width: u16,
+    device_info: Box<[RowItem]>,
+) -> io::Result<()> {
+    for row in device_info {
+        match row {
+            RowItem::KV(k, v) => {
+                output
+                    .queue(Print('│'))?
+                    .queue(MoveRight(1))?
+                    .queue(Print(k))?
+                    .queue(MoveToColumn(t_col))?
+                    .queue(Print('│'))?
+                    .queue(MoveRight(1))?
+                    .queue(PrintStyledContent(v.italic()))?
+                    .queue(MoveToColumn(width - 1))?
+                    .queue(Print('│'))?
+                    .queue(MoveToNextLine(1))?;
+            }
+            RowItem::LineBreak => {
+                output.queue(Print('├'))?;
+
+                for _i in 1..width - 1 {
+                    output.queue(Print('─'))?;
+                }
+
+                output.queue(Print('┤'))?;
+
+                output
+                    .queue(MoveToColumn(t_col))?
+                    .queue(Print('┼'))?
+                    .queue(MoveToNextLine(1))?;
+            }
+        }
+    }
+
     Ok(())
 }
